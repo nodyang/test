41a4b2ae1f564e56c8751ac80e64f68001efc707
我叫锄头 <nodyang@qq.com>
2023-12-02 18:17:18 +0800

    First

--- Ty/Program.cs
+++ Ty/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+
+
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
